4d7a0b970072aaa5de09b6825085cf13d660e4d1
UnrealDisaster <marius.hochwald@gmail.com>
2018-11-09 13:06:47 +0100

    Resolution Change

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main(){
 
     //std::cout << "gameEngine" << std::endl;
 
-    GameEngine* gameEng = new GameEngine("Bomberman++", 800 , 500, gameLogic);
+    GameEngine* gameEng = new GameEngine("Bomberman++", 1344 , 704, gameLogic);
 
     gameEng->start();
 
